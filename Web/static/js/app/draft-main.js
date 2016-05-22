385a3f789e85b03c8f8320f76d16f0054d2f66fc
Clayton Passmore <ctpassmore@gmail.com>
2016-05-21 22:23:31 -0400

    Changed how screen works

--- Web/static/js/app/draft-main.js
+++ Web/static/js/app/draft-main.js
@@ -4,15 +4,15 @@ define(function(require) {
     var DraftContainer = require('app/view/draft_container');
     var TitleSpan = require('app/view/title_span');
 
-    var title_screen = Screen.get_current();
-    var draft_screen = Screen.add_screen();
+    var screen_mgr = new Screen();
+    var title_screen = screen_mgr.get_current();
+    var draft_screen = screen_mgr.add_screen();
 
     title_span = new TitleSpan();
     title_span.set_title('Welcome to the Draft.');
     title_screen.appendChild(title_span.get_element());
 
-    title_screen.addEventListener('click', Screen.next);
-    title_screen.addEventListener('keypress', Screen.next);
+    title_screen.addEventListener('click', screen_mgr.next.bind(screen_mgr));
 
     sidebar = new SideBar();
     sidebar.hide();
