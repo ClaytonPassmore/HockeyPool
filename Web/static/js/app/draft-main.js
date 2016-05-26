65605764c19bb4d1f742398b4951e8bdd77da921
Clayton Passmore <ctpassmore@gmail.com>
2016-05-25 23:41:42 -0400

    Added event handlers for dialogue

--- Web/static/js/app/draft-main.js
+++ Web/static/js/app/draft-main.js
@@ -6,12 +6,26 @@ define(['app/view/screen_mgr', 'app/view/sidebar', 'app/view/draft_container', '
     var teams_screen = screen_mgr.add_screen();
     var draft_screen = screen_mgr.add_screen();
 
+    var event_queue = [];
+    var back_handler = function() {
+        if(event_queue.length > 0) {
+            event_queue.pop();
+        }
+        screen_mgr.previous();
+    };
+    var button_handler = function(event) {
+        event_queue.push(event);
+        screen_mgr.next();
+    };
+
     title_span = new TitleSpan('Welcome to the Draft');
     title_screen.appendChild(title_span.get_element());
     title_screen.addEventListener('click', screen_mgr.next.bind(screen_mgr));
 
     var input_props = {'type': 'number', 'min': '1', 'value': '10'};
     var num_teams_dialogue = new Dialogue('How many teams are there?', input_props);
+    num_teams_dialogue.add_back_listener(back_handler);
+    num_teams_dialogue.add_button_listener(button_handler);
     teams_screen.appendChild(num_teams_dialogue.get_element());
 
     sidebar = new SideBar();
